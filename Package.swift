95b329156423b345adb27e50db0aa523d7923606
Kilian Koeltzsch <me@kilian.io>
2018-07-05 14:49:08 +0200

    depend on Yams and AnyCodable

--- Package.swift
+++ Package.swift
@@ -5,9 +5,11 @@ let package = Package(
     name: "muninn",
     dependencies: [
         .package(url: "https://github.com/vapor/console.git", from: "3.0.0"),
+        .package(url: "https://github.com/jpsim/Yams.git", from: "1.0.0"),
+        .package(url: "https://github.com/Flight-School/AnyCodable.git", from: "0.1.0"),
     ],
     targets: [
-        .target(name: "MuninnCore", dependencies: []),
+        .target(name: "MuninnCore", dependencies: ["Yams", "AnyCodable"]),
         .target(name: "muninn", dependencies: ["MuninnCore", "Console"]),
         .testTarget(name: "MuninnTests", dependencies: ["MuninnCore"])
     ]
